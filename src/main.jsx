888f06150b7b87cb999cdcda2b4addb8ddf38192
unique mark <markunique72@gmail.com>
2026-07-24 14:42:47 +0100

    Changed the favicon to the school logo

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import faviconUrl from '../public/favicon.svg'
+import logoUrl from '../logo/ChatGPT Image Jun 9, 2026, 04_03_29 AM.png'
 import App from './App.jsx'
 
-document.querySelector('link[rel="icon"]')?.setAttribute('href', faviconUrl)
+document.querySelector('link[rel="icon"]')?.setAttribute('href', logoUrl)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
