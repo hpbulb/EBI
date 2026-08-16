9d484693bf10da5708ec491500a93b70e2ead67e
unique mark <markunique72@gmail.com>
2026-08-16 13:24:43 +0100

    feat: add SchoolFees component for admin navigation

---
+++ Student_Portal/Student_School_Fees_Payment.jsx
@@ -0,0 +1,12 @@
+export default function SchoolFees() {
+    const navStyle = `bg-blue-900 px-4 py-4 rounded-2xl mx-4 my-5 shadow-lg flex jusitfy-between items-center`
+    const textStyle = `text-md text-white font-sans font-semibold`
+
+    return(
+        <>
+            <nav className={`${navStyle}`}>
+                <div className={`${textStyle}`}>Welcome Admin!</div>
+            </nav>
+        </>
+    )
+}
